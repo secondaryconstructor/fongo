3360c284d8ad538c0872da6837fd4dfd92d932b7
William Delanoue <=>
2018-02-26 01:28:39 +0100

    refs #316: change structures

---
+++ src/test/java/com/github/fakemongo/FongoV3ServerVersion3_6Test.java
@@ -0,0 +1,29 @@
+package com.github.fakemongo;
+
+import com.mongodb.DuplicateKeyException;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.IndexOptions;
+import com.mongodb.connection.ServerVersion;
+import org.bson.Document;
+import org.junit.Test;
+
+public class FongoV3ServerVersion3_6Test extends AbstractFongoV3Test {
+
+  @Override
+  public ServerVersion serverVersion() {
+    return Fongo.V3_6_SERVER_VERSION;
+  }
+
+  @Test
+  public void insertOneWithDuplicateValueForUniqueColumn_throwsDuplicateKeyException() {
+    // Given
+    MongoCollection collection = newCollection();
+    collection.createIndex(new Document("a", 1), new IndexOptions().name("a").unique(true));
+    collection.insertOne(new Document("_id", 1).append("a", 1));
+    collection.insertOne(new Document("_id", 2).append("a", 2));
+
+    // When
+    exception.expect(DuplicateKeyException.class);
+    collection.findOneAndUpdate(docId(2), new Document("$set", new Document("a", 1)));
+  }
+}
