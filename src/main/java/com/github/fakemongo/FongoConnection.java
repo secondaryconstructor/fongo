3fd2af51ac1937c6ad01f7606325073f07e6d24d
William Delanoue <=>
2018-05-09 22:09:53 +0200

    correct compilation error

--- src/main/java/com/github/fakemongo/FongoConnection.java
+++ src/main/java/com/github/fakemongo/FongoConnection.java
@@ -610,7 +610,7 @@ public class FongoConnection implements Connection {
       }
     } else if (command.containsKey("delete")) {
       final FongoDBCollection dbCollection = (FongoDBCollection) db.getCollection(command.get("delete").asString().getValue());
-      List<BsonValue> documentsToDelete;
+      List<? extends BsonValue> documentsToDelete;
 
       if (payload != null) {
         if (payload.hasAnotherSplit()) {
